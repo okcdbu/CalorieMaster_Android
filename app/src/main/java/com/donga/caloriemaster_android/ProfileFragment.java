aa4751d0cfa111d1061b4dfce4810e7e9d99aeec
okcdbu <okcdbu@gmail.com>
2020-11-29 20:38:36 +0900

    Signout added!

--- app/src/main/java/com/donga/caloriemaster_android/ProfileFragment.java
+++ app/src/main/java/com/donga/caloriemaster_android/ProfileFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +17,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
-
+Button btnlogout;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -33,6 +36,15 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        btnlogout = v.findViewById(R.id.logout);
+        btnlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                ((MainActivity)getActivity()).loadFragment(LoginFragment.newInstance());
+            }
+        });
+        return v;
     }
 }
