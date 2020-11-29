12cedccb410b4e498142370bdb80a5ecfe8cfd56
okcdbu <okcdbu@gmail.com>
2020-11-29 16:24:35 +0900

    Fragment File added!

--- app/src/main/java/com/donga/caloriemaster_android/MainActivity.java
+++ app/src/main/java/com/donga/caloriemaster_android/MainActivity.java
@@ -1,15 +1,40 @@
 package com.donga.caloriemaster_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    Fragment Recipefragment,Homefragment,Profilefragment;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_recipe: getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,Recipefragment).commit();
+                return true;
+                case R.id.action_home: getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,Homefragment).commit();
+                return true;
+                case R.id.action_profile: getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,Profilefragment).commit();
+                return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Recipefragment = new RecipeFragment();
+        Homefragment = new HomeFragment();
+        Profilefragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,Homefragment).commit();
     }
 }
---
+++ app/src/main/java/com/donga/caloriemaster_android/RecipeFragment.java
@@ -0,0 +1,38 @@
+package com.donga.caloriemaster_android;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RecipeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RecipeFragment extends Fragment {
+
+    public RecipeFragment() {
+
+    }
+
+    public static RecipeFragment newInstance() {
+        RecipeFragment fragment = new RecipeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe, container, false);
+    }
+}
